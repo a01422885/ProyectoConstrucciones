6653d5562fd1c260413d3b100238e995ec2d1739
a01422885 <58478529+a01422885@users.noreply.github.com>
2019-12-03 13:01:44 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,30 +13,137 @@
 
 using namespace std;
 
+void menu(){
+    
+    //Imprime las opciones que va a tener el sistema
+    cout << "Menu:\n";
+    cout << "1. Mostrar trabajadores asalariados. \n";
+    cout << "2. Mostrar trabajadores por hora. \n";
+    cout << "3. Mostrar practicantes. \n";
+    cout << "4. Calcular total de salario de asalariados. \n";
+    cout << "5. Calcular total de salario de por hora \n";
+    cout << "6. Calcular total de salario de practicantes \n";
+    cout << "7. Calcular total que se reporta a hacienda \n";
+    cout << "8. Calcula total de egresos de salarios \n";
+    cout << "9. Agregar asalariados \n";
+    cout << "10. Agregar Por hora \n";
+    cout << "11. Agregar practicantes \n";
+    cout << "12. Salir \n";
+}
+
 
 
 int main(){
     
-    //debes crear los objetos luego luego después del main;
+        Construcciones EmpireState(310, 40000, 3500,10);
+        Construcciones Burj(600, 75000, 6500,10);
     
-    /*cout << "A continuación siguen ejemplos de complejos arquitectónicos conocidos";
-     //Caso sencillo de prueba para clase de Costos, aun debe de modificarse y aprender cosas nuevas para implementarse correctamente esta clase
-     Costos Prueba1(30,58);
-     cout << "\n" << "\n Costo de Energía por complejos arquitectónicos" << Prueba1.getCEE() << "\n" << "\n Costo de Agua por complejos arquitectónicos" << Prueba1.getCAE();*/
+    int opcion = 0;
+    
+    //Ciclo para que el sistema siga corriendo mientras no elija la opcion salir.
+    while(opcion < 12 && opcion > -1){
+        
+        //Impresion de menu
+        menu();
+        //Leer indice que selecciona la opcion del menu
+        cin >> opcion;
+        
+        //Switch donde dependiendo la eleccion efectua un diferente procedimiento
+        switch(opcion){
+                
+                //Caso 1 que imprime los empleados de tipo asalariado
+            case 1:
+                
+                cout << "\n" << "\n" << "Edificio: Empire State";
+                cout << "\n" << " El gasto de agua en litros es de : " << EmpireState.gastoAgua() << "\n";
+                cout << "\n" << " El gasto de energia en kWh es de : " << EmpireState.gastoEnergia() << "\n";
+                cout << "\n" << " Las toneladas de concreto ocupadas fueron: " << EmpireState.toneladas() << "\n";
+                
+                break;
+                
+                //Caso 2 que imprime los empleados de tipo Por Hora
+            case 2:
+                empresa.muestraPorHora();
+                
+                break;
+                
+                //Caso 3 que imprime los empleados de tipo Practicantes
+            case 3:
+                empresa.muestraPracticantes();
+                
+                break;
+                
+                //Caso 4 que imprime el total del salario de todos los Asalariados
+            case 4:
+                
+                empresa.pagoAsalariado();
+                break;
+                
+                //Caso 5 que imprime el total del salario de todos los PorHora
+            case 5:
+                empresa.pagoPorHora();
+                break;
+                
+                //Caso 6 que imprime el total del salario de todos los Practicantes
+            case 6:
+                empresa.pagoPracticantes();
+                break;
+                
+                //Caso 7 Calcula los salarios que se reportan a hacienda
+            case 7:
+                
+                empresa.pagoHacienda();
+                
+                break;
+                
+                //Caso 8 Calcula los salarios totales
+            case 8:
+                
+                empresa.pagoTotal();
+                
+                break;
+                
+            case 9:
+                cout<< "Dime el nombre " ;
+                cin >> temp_nombre;
+                cout<< "Dime el salario " ;
+                cin >> temp_salario;
+                empresa.agregaAsalariado(temp_nombre, temp_salario);
+                break;
+                
+            case 10:
+                cout<< "Dime el nombre " ;
+                cin >> temp_nombre;
+                cout<< "Dime el salario " ;
+                cin >> temp_salario;
+                cout<< "Dime las horas " ;
+                cin >> temp_horas;
+                empresa.agregaPorhora(temp_nombre, temp_horas, temp_salario);
+                break;
+                
+            case 11:
+                cout<< "Dime el nombre " ;
+                cin >> temp_nombre;
+                cout<< "Dime el salario " ;
+                cin >> temp_salario;
+                cout<< "Dime las horas " ;
+                cin >> temp_horas;
+                empresa.agregaPracticante(temp_nombre, temp_horas, temp_salario);
+                break;
+        }
+    }
+    
     
     //Casos de prueba para clase construcciones
-    Construcciones EmpireState(310, 40000, 3500,10);
-    //EmpireState.mostrarVariables2();
+    
     cout << "\n" << "\n" << "Edificio: Empire State";
     cout << "\n" << " El gasto de agua en litros es de : " << EmpireState.gastoAgua() << "\n";
     cout << "\n" << " El gasto de energia en kWh es de : " << EmpireState.gastoEnergia() << "\n";
     cout << "\n" << " Las toneladas de concreto ocupadas fueron: " << EmpireState.toneladas() << "\n";
     
-    cout << "Por último tenemos ejemplos de frutos que consumimos diariamente y sus puntajes de sustentabilidad";
+
     
     //Casos de prueba para clase construcciones
-    Construcciones Burj(600, 75000, 6500,10);
-    //Burj.mostrarVariables2();
     cout << "\n"<< "\n" << "Edificio: Burj Khalifa";
     cout << "\n" << " El gasto de agua en litros es de : " << Burj.gastoAgua() << "\n";
     cout << "\n" << " El gasto de energia en kWh es de : " << Burj.gastoEnergia() << "\n";
